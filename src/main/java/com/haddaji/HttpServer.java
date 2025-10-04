18af978f7b7ad5ff004bb55d60fcca1e1638fe5e
Haddajii <mohamedhaddaji68@gmail.com>
2025-10-04 22:13:24 +0100

    setting up the project

---
+++ src/main/java/com/haddaji/HttpServer.java
@@ -0,0 +1,10 @@
+package com.haddaji;
+
+//Driver class for the HTTP server
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("server starting...");
+
+
+    }
+}
