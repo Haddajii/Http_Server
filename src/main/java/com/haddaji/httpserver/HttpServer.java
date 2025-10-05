821165e0b8584fc2d41ba44b2367fae23b241ede
Haddajii <mohamedhaddaji68@gmail.com>
2025-10-05 13:34:55 +0100

    added basic server socket to send simple http response

--- src/main/java/com/haddaji/httpserver/HttpServer.java
+++ src/main/java/com/haddaji/httpserver/HttpServer.java
@@ -3,6 +3,13 @@ package com.haddaji.httpserver;
 import com.haddaji.httpserver.config.Configuration;
 import com.haddaji.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 //Driver class for the HTTP server
 public class HttpServer {
     public static void main(String[] args) {
@@ -11,5 +18,25 @@ public class HttpServer {
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("server started on port : " + conf.getPort() + " with webroot : " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept() ;
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Haddaji's Http Server</title></head><body><h1>This page was served using Haddaji's Http Server</h1></body></html>";
+            String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + html;
+            outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/haddaji/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/haddaji/httpserver/config/ConfigurationManager.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.haddaji.httpserver.util.json;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class ConfigurationManager {
@@ -16,7 +14,6 @@ public class ConfigurationManager {
 
     }
 
-
     public static ConfigurationManager getInstance() {
         if (myConfigurationManager == null) {
             myConfigurationManager = new ConfigurationManager();
@@ -26,32 +23,24 @@ public class ConfigurationManager {
 
     //used to load configuration file by the path provided
     public void loadConfigurationFile(String filePath)  {
-        FileReader filereader = null;
+        final String content;
         try {
-            filereader = new FileReader(filePath);
-        } catch (FileNotFoundException e) {
-            throw new HttpConfigurationException(e);
-        }
-        StringBuilder sb = new StringBuilder();
-        int i;
-        try {
-            while ((i = filereader.read()) != -1) {
-                sb.append((char) i);
-            }
+            content = java.nio.file.Files.readString(java.nio.file.Path.of(filePath));
         } catch (IOException e) {
             throw new HttpConfigurationException(e);
         }
 
-        JsonNode conf = null;
+        final JsonNode conf;
         try {
-            conf = json.parse(sb.toString());
+            conf = json.parse(content);
         } catch (IOException e) {
             throw new HttpConfigurationException("error parsing configuration file", e);
         }
+
         try {
             myCurrentConfiguration = json.fromJson(conf, Configuration.class);
         } catch (JsonProcessingException e) {
-            throw new HttpConfigurationException("error parsing configuration file , internal",e);
+            throw new HttpConfigurationException("error parsing configuration file, internal", e);
         }
     }
     //used to get the current configuration
