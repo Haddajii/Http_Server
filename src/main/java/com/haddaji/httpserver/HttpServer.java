940dae09ded582f36c930b8b90042f5fe8b7226b
Haddajii <mohamedhaddaji68@gmail.com>
2025-10-06 13:09:42 +0100

    add multi-threaded server listener and worker threads with logging

--- src/main/java/com/haddaji/httpserver/HttpServer.java
+++ src/main/java/com/haddaji/httpserver/HttpServer.java
@@ -2,41 +2,30 @@ package com.haddaji.httpserver;
 
 import com.haddaji.httpserver.config.Configuration;
 import com.haddaji.httpserver.config.ConfigurationManager;
+import com.haddaji.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+
 
 //Driver class for the HTTP server
 public class HttpServer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
     public static void main(String[] args) {
-        System.out.println("server starting...");
+        LOGGER.info("server starting...");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
-        System.out.println("server started on port : " + conf.getPort() + " with webroot : " + conf.getWebroot());
+        LOGGER.info("server started on port : {} with webroot : {}", conf.getPort(), conf.getWebroot());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept() ;
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Haddaji's Http Server</title></head><body><h1>This page was served using Haddaji's Http Server</h1></body></html>";
-            String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + html;
-            outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
---
+++ src/main/java/com/haddaji/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,46 @@
+package com.haddaji.httpserver.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class HttpConnectionWorkerThread extends Thread {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+
+    private Socket socket;
+    public HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    InputStream inputStream = null;
+    OutputStream outputStream = null;
+
+    @Override
+    public void run() {
+        try {
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Haddaji's Http Server</title></head><body><h1>This page was served using Haddaji's Http Server</h1></body></html>";
+            String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + html;
+            outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
+
+
+            LOGGER.info("connection processing finished ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                if(inputStream != null) inputStream.close();
+                if(outputStream != null) outputStream.close();
+                if(socket != null && !socket.isClosed()) socket.close();
+            } catch (IOException e) {}
+        }
+    }
+}
