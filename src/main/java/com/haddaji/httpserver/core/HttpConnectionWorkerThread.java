762854ff8b7bf3bf49344aec34b39de25cd8b2f7
Haddajii <mohamedhaddaji68@gmail.com>
2025-10-06 23:20:50 +0100

    added HttpParser skeleton + first unit test with fake GET request

--- src/main/java/com/haddaji/httpserver/core/HttpConnectionWorkerThread.java
+++ src/main/java/com/haddaji/httpserver/core/HttpConnectionWorkerThread.java
@@ -26,6 +26,7 @@ public class HttpConnectionWorkerThread extends Thread {
             inputStream = socket.getInputStream();
             outputStream = socket.getOutputStream();
 
+
             String html = "<html><head><title>Haddaji's Http Server</title></head><body><h1>This page was served using Haddaji's Http Server</h1></body></html>";
             String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + html;
             outputStream.write(httpResponse.getBytes(StandardCharsets.UTF_8));
@@ -40,7 +41,9 @@ public class HttpConnectionWorkerThread extends Thread {
                 if(inputStream != null) inputStream.close();
                 if(outputStream != null) outputStream.close();
                 if(socket != null && !socket.isClosed()) socket.close();
-            } catch (IOException e) {}
+            } catch (IOException e) {
+                LOGGER.error("error closing connection", e);
+            }
         }
     }
 }
---
+++ src/main/java/com/haddaji/httpserver/http/HttpParser.java
@@ -0,0 +1,14 @@
+package com.haddaji.httpserver.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+public class HttpParser {
+    private final Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
+
+    public void parseHttpRequest(InputStream inputStream) {
+
+    }
+}
